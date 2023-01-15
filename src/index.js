dfda7272b70bd732982ce1f197028ce0eb4f2f4f
CloudMojos <joshuel.simbulan@gmail.com>
2023-01-15 23:00:31 +0800

    finish webpack lecture

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import printMe from './print.js';
 
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   btn.innerHTML = 'Click me and check the console';
+   btn.innerHTML = 'Click me and <ctrl+shift+i>';
    btn.onclick = printMe;
 
    element.appendChild(btn);
